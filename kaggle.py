aaf6b63e952ff1b2ce2aee8f38396452d66a4552
unknown <andrebeckpires@gmail.com>
2025-04-08 19:58:50 -0300

    criaçao da label categorica

---
+++ kaggle.py
@@ -0,0 +1,47 @@
+# Install dependencies as needed:
+# pip install kagglehub[pandas-datasets]
+import kagglehub
+from kagglehub import KaggleDatasetAdapter
+import pandas as pd
+
+# Set the path to the file you'd like to load
+file_path = "brasileirao2.xlsx"
+
+def load_data():
+# Load the latest version
+  global df
+  df = kagglehub.load_dataset(
+    KaggleDatasetAdapter.PANDAS,
+    "regazze/brasileirao-2003-2022",
+    file_path,
+    # Provide any additional arguments like 
+    # sql_query or pandas_kwargs. See the 
+    # documenation for more information:
+    # https://github.com/Kaggle/kagglehub/blob/main/README.md#kaggledatasetadapterpandas
+  )
+
+
+def selecionar_atual():
+  global formato_atual
+  formato_atual = df[df['year'] >= 2006]
+
+def classificar(): 
+  print(df.shape)  
+  bins = [0,4,6,12,16,20]
+  labels = ["Libertadores","Pre-libertadores", "Sul-Americana", "Limbo","Rebaixamento"]
+  formato_atual["Classificacao"] = pd.cut(formato_atual['position'], bins=bins, labels=labels, include_lowest=True)
+
+def save_to_csv():
+  output_file = "brasileirao_formato_atual_data.csv"
+  formato_atual.to_csv(output_file, index=False, encoding='utf-8')
+  print(f"Dataset salvo com sucesso em: {output_file}")
+  output_file = "dataset_completo.csv"
+  df.to_csv(output_file, index=False, encoding='utf-8')
+  print(f"Dataset salvo com sucesso em: {output_file}")
+
+
+load_data()
+selecionar_atual()
+classificar()
+save_to_csv()
+print("First 5 records:", formato_atual.head())
